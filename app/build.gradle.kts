c5a31abeee651abba5ac2990c78e92df4df3a273
Верховод Никита Сергеевич <nsverhovod@mts.ru>
2024-11-08 19:18:40 +0300

    add gradle plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,14 @@
 plugins {
-    id("java")
+    application
+    checkstyle
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
+application {
+    mainClass.set("hexlet.code.App")
+}
 repositories {
     mavenCentral()
 }
