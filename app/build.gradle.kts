c3ab6d1e00d31ac53024d4b925a8697597045cbc
Верховод Никита Сергеевич <nsverhovod@mts.ru>
2024-11-09 02:15:35 +0300

    add MAkeFile and picocli

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.7.6")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.6")
 }
 
 tasks.test {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,11 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+
+@CommandLine.Command(mixinStandardHelpOptions = true, version = "v0.0.1")
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
